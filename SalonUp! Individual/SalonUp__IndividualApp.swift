6dcfa8e3f6dc06f7f30e959843e3bd85abf406e3
Doğukan Çatmakaş <dcatmakas@gmail.com>
2023-05-16 23:56:45 +0300

    Initial Commit

---
+++ SalonUp! Individual/SalonUp__IndividualApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalonUp__IndividualApp.swift
+//  SalonUp! Individual
+//
+//  Created by Doğukan Çatmakaş on 16.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SalonUp__IndividualApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
